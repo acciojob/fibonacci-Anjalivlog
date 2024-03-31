d01bf43d52da731fa0caa751d1b0ebfe78f6d092
Anjalivlog <139326744+Anjalivlog@users.noreply.github.com>
2024-03-31 13:23:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function fibonacci(num) {
 // your code here
-	if (num == 1) {
-        return 0;
+	if (num <= 0) return 0;
+    if (num === 1) return 0;
+    let fib = [0, 1];
+    for (let i = 2; i <= num; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
     }
-    if (num == 2) {
-        return 1;
-    }
-    return fibonacci(num - 1) + fibonacci(num - 2);
+    return fib[num - 1];
 }
 
 module.exports = fibonacci;
